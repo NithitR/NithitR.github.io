55259ed68ea9524eb9a193d8cb156913b8aa4723
Nithit Rakkijsiri <fulfillmyself2@gmail.com>
2024-07-16 15:04:43 +0700

    optimise image, handle image load event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,19 @@
 
+var initialise = false;
+var i = 0;
+var txt = `With joyful hearts, we invite you to celebrate the wedding of Wasunthara and Nithit. Join us as we embark on this beautiful journey together.` ; /* The text */
+var speed = 50;
+
 $(document).ready(function() {
-    var initialise = false;
 
-    function reveal() {
-        var reveals = document.querySelectorAll(".reveal");
-// console.log(reveals)
-
-        for (var i = 0; i < reveals.length; i++) {
-            var windowHeight = window.innerHeight;
-            var elementTop = reveals[i].getBoundingClientRect().top;
-            var elementVisible = 150;
-
-            if (elementTop < windowHeight - elementVisible) {
-                reveals[i].classList.add("active");
-            } else {
-                reveals[i].classList.remove("active");
-            }
-        }
+    var img = document.querySelector('#hero-image')
+    if (img.complete) {
+        loaded();
+    } else {
+        img.addEventListener('load', loaded)
     }
-
-    var i = 0;
-    var txt = `With joyful hearts, we invite you to celebrate the wedding of Wasunthara and Nithit. Join us as we embark on this beautiful journey together.` ; /* The text */
-    var speed = 50; /* The speed/duration of the effect in milliseconds */
-    if(!initialise){
-        typeWriter();
-    }
-
-    function typeWriter() {
-        if (i < txt.length) {
-            initialise = true
-            document.getElementById("text").innerHTML += txt.charAt(i);
-            i++;
-            setTimeout(typeWriter, speed);
-        }else {
-            // initialise = true;
-        }
-    }
-
     window.addEventListener("scroll", reveal);
+
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
         if (scroll >= 1) {
@@ -49,6 +25,41 @@ $(document).ready(function() {
 });
 
 
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+// console.log(reveals)
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+
+function typeWriter() {
+    if (i < txt.length) {
+        initialise = true
+        document.getElementById("text").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }else {
+        // initialise = true;
+    }
+}
+
+function loaded() {
+    if(!initialise){
+        typeWriter();
+    }
+}
 // const gra = function(min, max) {
 //     return Math.random() * (max - min) + min;
 // }
