55708fee962a50edc0bb1ab98860a90c26ca26bf
Nithit Rakkijsiri <fulfillmyself2@gmail.com>
2024-08-25 16:25:05 +0700

    latest

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,35 @@
 
 $(document).ready(function() {
-    var initialise = false;
-
-    function reveal() {
-        var reveals = document.querySelectorAll(".reveal");
-// console.log(reveals)
-
-        for (var i = 0; i < reveals.length; i++) {
-            var windowHeight = window.innerHeight;
-            var elementTop = reveals[i].getBoundingClientRect().top;
-            var elementVisible = 150;
-
-            if (elementTop < windowHeight - elementVisible) {
-                reveals[i].classList.add("active");
-            } else {
-                reveals[i].classList.remove("active");
-            }
-        }
-    }
-
-    var i = 0;
-    var txt = `With joyful hearts, we invite you to celebrate the wedding of Wasunthara and Nithit. Join us as we embark on this beautiful journey together.` ; /* The text */
-    var speed = 50; /* The speed/duration of the effect in milliseconds */
-    if(!initialise){
-        typeWriter();
-    }
-
-    function typeWriter() {
-        if (i < txt.length) {
-            initialise = true
-            document.getElementById("text").innerHTML += txt.charAt(i);
-            i++;
-            setTimeout(typeWriter, speed);
-        }else {
-            // initialise = true;
-        }
-    }
-
     window.addEventListener("scroll", reveal);
+
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
         if (scroll >= 1) {
-            $('.arrow').addClass('fade');
+            $('.arrow-down').addClass('fade');
         } else{
-            $('.arrow').removeClass('fade');
+            $('.arrow-down').removeClass('fade');
         }
     })
 });
+$(function() {
+    $('a[href*=#]').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+});
 
 
-// const gra = function(min, max) {
-//     return Math.random() * (max - min) + min;
-// }
-// const init = function(){
-//     let items = document.querySelectorAll('section');
-//     for (let i = 0; i < items.length; i++){
-//         items[i].style.background = randomColor({luminosity: 'light'});
-//     }
-//     cssScrollSnapPolyfill()
-// }
-// init();
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
